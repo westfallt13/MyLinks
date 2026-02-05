42fcf466cc9da2b13ce72fbafdb2218fb8777dc9
westfallt13 <westfallt13@gmail.com>
2026-02-05 11:59:20 -0600

    Updated Portfolio section and app.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,38 +1,43 @@
 import { Link } from 'react-router-dom';
 
 function Portfolio() {
-  return (
-    <main className='container'>
-      <h1 className='mt-3'>My Portfolio</h1>
-      
-      <ul className="list-group mb-4">
+    return (
+        <main className='container'>
+            <h1 className='mt-3'>My Portfolio</h1>
 
-        <li className="list-group-item mb-1 rounded">
-          <a href="https://forgeonapp.netlify.app/" target="_blank" rel="noopener noreferrer">
-            Forgeon Game Planner
-          </a>
-        </li>
+            <ul className="list-group mb-4">
 
+                <li className="list-group-item mb-1 rounded">
+                    <a href="https://forgeonapp.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        Forgeon Game Planner
+                    </a>
+                </li>
 
+                <li className="list-group-item mb-1 rounded">
+                    <a href="https://multiaccountexpensetracker.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        Multi Account Budget Tracker
+                    </a>
+                </li>
 
-        <li className="list-group-item mb-1 rounded">
-          <a href="https://pokemonapidemoapp.netlify.app/" target="_blank" rel="noopener noreferrer">
-            Pokemon API Demo App
-          </a>
-        </li>
-        
-        <li className="list-group-item mb-1 rounded">
-          <a href="https://latlonweather1.netlify.app/" target="_blank" rel="noopener noreferrer">
-            Simple Latitude/Longitude Temperature Checker
-          </a>
-        </li>
-      </ul>
+                <li className="list-group-item mb-1 rounded">
+                    <a href="https://pokemonapidemoapp.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        Pokemon API Demo App
+                    </a>
+                </li>
 
-      <Link to="/" className="btn btn-secondary">
-        ← Back to Links
-      </Link>
-    </main>
-  );
+                <li className="list-group-item mb-1 rounded">
+                    <a href="https://latlonweather1.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        Simple Latitude/Longitude Temperature Checker
+                    </a>
+                </li>
+
+            </ul>
+
+            <Link to="/" className="btn btn-secondary">
+                ← Back to Links
+            </Link>
+        </main>
+    );
 }
 
 export default Portfolio;
