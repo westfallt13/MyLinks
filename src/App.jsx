acef23b52486da47bbad02a13af847401cee032c
westfallt13 <westfallt13@gmail.com>
2026-02-11 15:35:06 -0600

    Added hamburger dropdown menu for mobile devices.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
@@ -7,14 +8,29 @@ import { ContactForm } from './pages/ContactForm'
 import Portfolio from './pages/Portfolio'
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  const closeMenu = () => {
+    setIsMenuOpen(false)
+  }
+
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/" className="nav-button">Home</Link>
-        <Link to="/about" className="nav-button">About Me</Link>
-        <Link to="/vision" className="nav-button">Mission & Vision</Link>
-        <Link to="/portfolio" className="nav-button">My Portfolio</Link>
-        <Link to="/contact" className="nav-button">Contact Me</Link>
+        <button className="hamburger" onClick={toggleMenu}>
+          ☰
+        </button>
+        <div className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+          <Link to="/" className="nav-button" onClick={closeMenu}>Home</Link>
+          <Link to="/about" className="nav-button" onClick={closeMenu}>About</Link>
+          <Link to="/vision" className="nav-button" onClick={closeMenu}>Vision</Link>
+          <Link to="/portfolio" className="nav-button" onClick={closeMenu}>Portfolio</Link>
+          <Link to="/contact" className="nav-button" onClick={closeMenu}>Contact</Link>
+        </div>
       </nav>
 
       <Routes>
